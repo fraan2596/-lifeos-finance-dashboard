8507253e69e2d62690fcf5a62f5d97e08a81f9d7
fraan2596 <franllobregat251@gmail.com>
2026-07-03 22:02:45 +0200

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,6 +1,20 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    prueba: "FUNCIONA",
-    fecha: new Date().toISOString()
-  });
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export default async function handler(req, res) {
+  try {
+    const response = await notion.search({
+      page_size: 100,
+    });
+
+    return res.status(200).json(response);
+
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
 }
