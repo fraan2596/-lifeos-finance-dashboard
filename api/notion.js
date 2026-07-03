0033f5cc829c16c3e6cb48586fa726872f3c72e1
fraan2596 <franllobregat251@gmail.com>
2026-07-03 17:34:58 +0200

    Update notion.js
    Backend API

--- api/notion.js
+++ api/notion.js
@@ -1,6 +1,20 @@
 export default async function handler(req, res) {
-  res.status(200).json({
-    status: "OK",
-    mensaje: "LifeOS Dashboard funcionando correctamente 🚀"
-  });
+
+  try {
+
+    return res.status(200).json({
+      ok: true,
+      message: "API de LifeOS funcionando",
+      version: "1.0.0"
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      ok: false,
+      error: error.message
+    });
+
+  }
+
 }
